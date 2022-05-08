9b33f1397999fff48a08ff6be255e6799e7ef881
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-05-08 16:23:31 +0200

    feat: video type for search results

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,6 +4,7 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: process.env.YOUTUBE_API_KEY
   }
