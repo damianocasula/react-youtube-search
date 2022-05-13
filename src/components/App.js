5d062964a240b40194ebb14e40c9ccf5cfc54b84
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-05-13 12:21:03 +0200

    feat: video detail component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import youtube from '../apis/youtube'
 
 import SearchBar from './SearchBar'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null }
@@ -26,6 +27,7 @@ class App extends React.Component {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
