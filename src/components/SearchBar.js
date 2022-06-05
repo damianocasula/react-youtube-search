c041513c73b9440dc4b725ce9957bf0fbbce2536
damianocasula <95046135+damianocasula@users.noreply.github.com>
2022-06-05 17:34:16 +0200

    fix: edit search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,21 @@ import React, { useState } from 'react'
 const SearchBar = ({ onFormSubmit }) => {
   const [term, setTerm] = useState('')
 
-  const onInputChange = event => {
-    setTerm(event.target.value)
-  }
-
   const onSubmit = event => {
     event.preventDefault()
-
     onFormSubmit(term)
   }
 
   return (
     <div className='search-bar ui segment'>
-      <form onSubmit={onFormSubmit} className='ui form'>
+      <form onSubmit={onSubmit} className='ui form'>
         <div className='field'>
           <label>Video Search</label>
-          <input type='text' value={term} onChange={onInputChange} />
+          <input
+            type='text'
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
         </div>
       </form>
     </div>
